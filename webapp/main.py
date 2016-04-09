68ba8b213bbff799c0c62fa605fe3e5ea006911b
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-04-09 11:31:33 -0500

    server activate

--- webapp/main.py
+++ webapp/main.py
@@ -1,26 +1,18 @@
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 import webapp2
 from webapp2_extras import jinja2
 
-
 class BaseHandler(webapp2.RequestHandler):
-    def render_template(self, template_name):d
+    def render_template(self, template_name):
         renderer = jinja2.get_jinja2(app=app)
         template = renderer.render_template(template_name)
         self.response.write(template)
-
 
 class LandingHandler(BaseHandler):
 
     def get(self):
         self.render_template('landing.html')
 
-
-class MapsHandler(BaseHandler):
-
-    def get(self):
-        self.render_template('maps.html')
 
 
 app = webapp2.WSGIApplication([
@@ -29,8 +21,4 @@ app = webapp2.WSGIApplication([
                   name='landing_handler',
                   methods=['GET']),
 
-    webapp2.Route(r'/maps',
-                  handler=MapsHandler,
-                  name='maps_handler',
-                  methods=['GET']),
 ], debug=True)
