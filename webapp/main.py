ea2039165c799247d711505cea542f9a0e0dd3ea
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-04-10 05:29:22 -0500

    Create SmsReceive Class.

--- webapp/main.py
+++ webapp/main.py
@@ -9,14 +9,32 @@ class BaseHandler(webapp2.RequestHandler):
         self.response.write(template)
 
 class LandingHandler(BaseHandler):
-
-    def get(self):
+     def get(self):
         self.render_template('landing.html')
+
 
 class EventsHandler(BaseHandler):
     def get(self):
         self.render_template('events.html')
 
+
+class SmsReceive(webapp2.RequestHandler):
+    def receive_sms(self):
+
+        from_number = self.RequestHandler.get('From')
+
+
+        to_number = self.RequestHandler.get('To')
+
+
+        text = self.RequestHandler.get('Text')
+
+        print 'Text received: %s - From: %s' % (text, from_number)
+        return "Text received"
+
+    if __name__ == '__main__':
+        app_sms.run(host='0.0.0.0',
+                    debug=True)
 
 
 
