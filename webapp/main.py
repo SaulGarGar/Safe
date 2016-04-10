babe8d6aaa49c4fea09436b2e86e4f8a7a91f7e8
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-04-10 13:50:30 -0500

    Add Plivo on Flask.

--- webapp/main.py
+++ webapp/main.py
@@ -2,40 +2,22 @@ import os
 import webapp2
 from webapp2_extras import jinja2
 
+
 class BaseHandler(webapp2.RequestHandler):
     def render_template(self, template_name):
         renderer = jinja2.get_jinja2(app=app)
         template = renderer.render_template(template_name)
         self.response.write(template)
 
+
 class LandingHandler(BaseHandler):
-     def get(self):
+    def get(self):
         self.render_template('landing.html')
 
 
 class EventsHandler(BaseHandler):
     def get(self):
         self.render_template('events.html')
-
-
-class SmsReceive(webapp2.RequestHandler):
-    def receive_sms(self):
-
-        from_number = self.RequestHandler.get('From')
-
-
-        to_number = self.RequestHandler.get('To')
-
-
-        text = self.RequestHandler.get('Text')
-
-        print 'Text received: %s - From: %s' % (text, from_number)
-        return "Text received"
-
-    if __name__ == '__main__':
-        app_sms.run(host='0.0.0.0',
-                    debug=True)
-
 
 
 app = webapp2.WSGIApplication([
